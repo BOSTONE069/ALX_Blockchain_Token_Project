2aad229d85825976cba7ecf175a34e412aeacd9f
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-12 05:10:51 -0400

    This is the alx token project token build on sepolia test ne

---
+++ alxtoken.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// imports
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract alxToken is Ownable {
+using SafeMath for uint256;
+
+
+        // global Var
+        string public tokenName = "ALX TOKEN";
+        string public symbol = "ALX";
+        uint public decimals = 18; // 0.000000000000000009 // 9000000000000000000
+        uint public decimalFactor = 10 ** decimals; // 10^18 10^2 0.00
+        uint public totalSupply = 1000000 * decimalFactor; // 0.00000000001
+
+        event Transfer(address indexed _from, address indexed _to, uint256 _amount);
+        event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+        // mapping
+        mapping(address => uint256) public balanceOf;
+        mapping(address => mapping(address => uint256)) public allowance;
+
+        constructor() {
+            balanceOf[msg.sender] = totalSupply;
+        }
+
+
+        function transfer(address _to, uint256 _value) public returns (bool success) {
+            require(balanceOf[msg.sender] >= _value);
+            balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+            balanceOf[_to] = balanceOf[_to].add(_value);
+            emit Transfer(msg.sender, _to, _value);
+            return true;
+        }
+
+
+        function approve(address _spender, uint256 _value) public returns (bool success) {
+            allowance[msg.sender][_spender] = _value;
+            emit Approval(msg.sender, _spender, _value);
+            return true;
+        }
+
+
+        function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+            require(_value <= balanceOf[_from]);
+            require(_value <= allowance[_from][msg.sender]);
+            balanceOf[_from] = balanceOf[_from].sub(_value);
+            balanceOf[_to] = balanceOf[_to].add(_value);
+            allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+            emit Transfer(_from, _to, _value);
+            return true;
+        }
+
+
+        function mintToken(uint256 _amount) public onlyOwner returns (bool success) {
+            totalSupply = totalSupply.add(_amount);
+            balanceOf[msg.sender] = balanceOf[msg.sender].add(_amount);
+            emit Transfer(address(0), msg.sender, _amount);
+            return true;
+        }
+}
---
+++ index.js
@@ -0,0 +1,35 @@
+const Web3 = require('web3');
+const sh256 = require("crypto-js/sha256");
+const bip39 = require('bip39');
+
+
+//address: '0xE231995Dd00F2ceB6e1aCD98dC81e54b73B38C1D',
+//privateKey: '0x5d9dfe8f7fbf72df78369b1eda5030c380e8d6a8cd3ac948844695f28cf2b873'
+
+const web3 = new Web3('https://eth-sepolia.g.alchemy.com/v2/M6m7lD2AmuRs4mJepYbATpe_0Vm_gQNG');
+
+//get balance
+web3.eth.getBalance("0xe4F509254fe8e5fa65E620De7525890421Ad003f", (err, wei) => {
+  balance = web3.utils.fromWei(wei, "ether")
+  console.log("balance", balance)
+})
+
+
+//get accounts
+web3.eth.getAccounts().then(accounts => {
+  console.log("Accounts: ", accounts);
+}).catch(err => {
+  console.log("Error getting accounts: ", err);
+});
+
+const getWalletBalance = async (_address) => {
+
+  // check if wallet address is valid
+  if (!web3.utils.isAddress(_address)){
+    throw new error("Invalid wallet address")
+  }
+
+  const balance = await web3.eth.getBalance(_address)
+  return web3.utils.fromWei(balance, "ether")
+}
+getWalletBalance("0x8D5FFFB23e9D70F2DE92c154735022F468e6Bd16").then(console.log)
