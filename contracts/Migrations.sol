311575956c6dc7bf46b3b0c403fe82ca0c7765b3
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-12 05:38:25 -0400

    Changed the truffle version and compiled the contratc using truffle

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.22 <0.7.0;
+pragma solidity ^0.8.9;
 
 contract Migrations {
   address public owner;
--- truffle-config.js
+++ truffle-config.js
@@ -2,13 +2,14 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 7545,
       network_id: "*", // Match any network id
       gas: 5000000
     }
   },
   compilers: {
     solc: {
+      version:"^0.8.9",
       settings: {
         optimizer: {
           enabled: true, // Default: false
