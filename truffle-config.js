6085302cf6e462cfa7844569b1add538891f1b3e
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-17 12:53:43 -0400

    Modified the triffle config file to deploy through the sepolia test net and included the dotenv in the package.json

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,8 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require("dotenv").config()
+
+const { PROJECT_ID, MNEMONIC} = process.env;
+
 module.exports = {
   networks: {
     development: {
@@ -5,8 +10,24 @@ module.exports = {
       port: 7545,
       network_id: "*", // Match any network id
       gas: 5000000
-    }
+    },
+
+    //sepolia network
+  sepolia: {
+    provider: () => new HDWalletProvider(MNEMONIC, `https://eth-sepolia.g.alchemy.com/v2/${PROJECT_ID}`),
+    network_id: 11155111,       // Sepolia id
   },
+
+  mainnet: {
+    provider: () => new HDWalletProvider(MNEMONIC, `https://eth-mainnet.g.alchemy.com/v2/${PRODUCT_ID_MAINNET}`),
+    network_id: 1,
+    confimation: 2,
+    timeoutBlocks: 200,
+    skipDryRun: true
+  }
+
+  },
+  
   compilers: {
     solc: {
       version:"^0.8.9",
